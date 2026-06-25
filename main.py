f02be07f78aad021f65ebaa25ecf3f1d514208aa
kerlosmelad12 <kerlosmelad124@gmail.com>
2026-06-25 12:24:14 +0300

    update changes in main and requirments and base

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
-app=FastAPI()
+from routes import base
+from dotenv import load_dotenv
+load_dotenv('.env')
+import uvicorn
 
-@app.get("/welcome")
-def welcom():
-    return {"message":"Welcome to the mini-RAG application!"}
+
+app = FastAPI()
+app.include_router(base.base_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, reload=True)
--- routes/base.py
+++ routes/base.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI
-app=FastAPI()
+from fastapi import APIRouter
+import os
+base_router = APIRouter(
+    prefix="/MiniRAG-V1",
+    tags=['api_v1']
+)
 
-@app.get("/")
-def welcom():
-    return {"message":"Welcome to the mini-RAG application!"}
+@base_router.get("/")
+async def welcome():
+    app_name=os.getenv('APP_NAME')
+    version=os.getenv('APP_VERSION')
+    return {"app_name": app_name,
+            "app_version":version}
