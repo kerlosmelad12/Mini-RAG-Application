b4dd02232433834bb1a3b565064fd5911ce3f92c
kerlosmelad12 <kerlosmelad124@gmail.com>
2026-07-21 16:43:25 +0300

    Add  LLM provider structure; update settings and introduce new models for generation and embedding

--- src/helper/config.py
+++ src/helper/config.py
@@ -12,6 +12,22 @@ class Settings(BaseSettings):
     MONGODB_URI:str
     MONGODB_DB_NAME:str
 
+    
+    GENERATION_BACKEND :str
+    EMBEDDING_BACKEND :str
+
+    GENERATION_MODEL:str=None
+    EMBEDDING_MODEL:str=None
+
+    COHERE_API_KEY:str=None
+    GEMINI_API_KEY:str=None
+
+
+    EMBEDDING_MODEL_SIZE:int=None
+    INPUT_DAFAULT_MAX_CHARACTERS:int=None
+    GENERATION_DAFAULT_MAX_TOKENS:int=None
+    GENERATION_DAFAULT_TEMPERATURE:float=None
+
     model_config = SettingsConfigDict(env_file=".env")
 
 def get_settings():
